72a0a33a23f30bbe2706a39656453482e2053f39
sho <>
2018-11-11 16:14:06 +0900

    add bootstrap template.

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+	<meta charset="utf-8">
+
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<!-- CSRF Token -->
+	<meta name="csrf-token" content="{{ csrf_token() }}">
+
+	<title>Laravel | @yield('title', 'Home')</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Free HTML5 Website Template by FreeHTML5.co" />
+	<meta name="keywords" content="free website templates, free html5, free template, free bootstrap, free website template, html5, css3, mobile first, responsive" />
+	<meta name="author" content="FreeHTML5.co" />
+
+	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content="" />
+	<meta property="og:image" content="" />
+	<meta property="og:url" content="" />
+	<meta property="og:site_name" content="" />
+	<meta property="og:description" content="" />
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<link href="https://fonts.googleapis.com/css?family=Raleway:100,300,400,700" rel="stylesheet">
+
+
+	<!-- Fonts -->
+	<link rel="dns-prefetch" href="https://fonts.gstatic.com">
+
+	<!-- Styles -->
+
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="{{ asset('css/animate.css') }}">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="{{ asset('css/icomoon.css') }}">
+	<!-- Themify Icons-->
+	<link rel="stylesheet" href="{{ asset('css/themify-icons.css') }}">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
+
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="{{ asset('css/magnific-popup.css') }}">
+
+	<!-- Owl Carousel  -->
+	<link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}">
+	<link rel="stylesheet" href="{{ asset('css/owl.theme.default.min.css') }}">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+	<!-- Modernizr JS -->
+	<script src="{{ asset('js/modernizr-2.6.2.min.js') }}"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+    <![endif]-->
+	<!-- jQuery -->
+	<script src="{{ asset('js/jquery.min.js') }}"></script>
+</head>
+
+<body>
+	<div class="gtco-loader"></div>
+
+	<div id="page">
+
+		<nav class="gtco-nav" role="navigation">
+			<div class="gtco-container">
+
+				<div class="row">
+					<div class="col-sm-2 col-xs-12">
+						<div id="gtco-logo"><a href="index.html"><img src="images/logo.png" alt="Free HTML5 Website Template by FreeHTML5.co"></a></div>
+					</div>
+					<div class="col-xs-10 text-right menu-1">
+						<ul>
+							<li><a href="{{ url('/') }}">Home</a></li>
+							<li><a href="about.html">About</a></li>
+							<li class="has-dropdown">
+								<a href="services.html">Services</a>
+								<ul class="dropdown">
+									<li><a href="#">Web Design</a></li>
+									<li><a href="#">eCommerce</a></li>
+									<li><a href="#">Branding</a></li>
+									<li><a href="#">API</a></li>
+								</ul>
+							</li>
+							<li class="has-dropdown">
+								<a href="#">Dropdown</a>
+								<ul class="dropdown">
+									<li><a href="#">HTML5</a></li>
+									<li><a href="#">CSS3</a></li>
+									<li><a href="#">Sass</a></li>
+									<li><a href="#">jQuery</a></li>
+								</ul>
+							</li>
+							<li><a href="portfolio.html">Portfolio</a></li>
+							<li><a href="contact.html">Contact</a></li>
+						</ul>
+					</div>
+				</div>
+
+			</div>
+		</nav>
+
+		@yield('contents')
+		<!-- END #gtco-header -->
+
+		<footer id="gtco-footer" class="gtco-section" role="contentinfo">
+			<div class="gtco-container">
+				<div class="row row-pb-md">
+					<div class="col-md-4 gtco-widget gtco-footer-paragraph">
+						<h3>sotsu</h3>
+						<p>イケイケサービス</p>
+					</div>
+					<div class="col-md-4 gtco-footer-link">
+						<div class="row">
+							<div class="col-md-6">
+								<ul class="gtco-list-link">
+									<li><a href="{{ url('/') }}">Home</a></li>
+									<li><a href="#">Features</a></li>
+									<li><a href="#">Products</a></li>
+									<li><a href="#">Testimonial</a></li>
+									<li><a href="#">Contact</a></li>
+								</ul>
+							</div>
+							<div class="col-md-6">
+								<p>
+									<a href="#">mail@mail.com</a>
+								</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 gtco-footer-subscribe">
+						<!-- <form class="form-inline">
+							<div class="form-group">
+								<label class="sr-only" for="exampleInputEmail3">Email address</label>
+								<input type="email" class="form-control" id="" placeholder="Email">
+							</div>
+							<button type="submit" class="btn btn-primary">Send</button>
+						</form> -->
+					</div>
+				</div>
+			</div>
+			<div class="gtco-copyright">
+				<div class="gtco-container">
+					<div class="row">
+						<div class="col-md-6 text-left">
+							<p><small>&copy; 2018 sotsu. </small></p>
+						</div>
+						<div class="col-md-6 text-right">
+							<p><small>Designed by <a href="http://freehtml5.co/" target="_blank">FreeHTML5.co</a> Demo Images: <a href="http://pixeden.com/"
+									 target="_blank">Pixeden</a> &amp; <a href="http://unsplash.com" target="_blank">Unsplash</a></small> </p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+	</div>
+
+	<!-- jQuery Easing -->
+	<script src="{{ asset('js/jquery.easing.1.3.js') }}"></script>
+	<!-- Bootstrap -->
+	<script src="{{ asset('js/bootstrap.min.js') }}"></script>
+	<!-- Waypoints -->
+	<script src="{{ asset('js/jquery.waypoints.min.js') }}"></script>
+	<!-- Carousel -->
+	<script src="{{ asset('js/owl.carousel.min.js') }}"></script>
+	<!-- Magnific Popup -->
+	<script src="{{ asset('js/jquery.magnific-popup.min.js') }}"></script>
+	<script src="{{ asset('js/magnific-popup-options.js') }}"></script>
+	<!-- Main -->
+	<script src="{{ asset('js/main.js') }}"></script>
+</body>
+
+</html>
---
+++ resources/views/room/index.blade.php
@@ -0,0 +1,102 @@
+@extends('layouts.app')
+
+@section('search room')
+Main Page
+@endsection
+
+@section('contents')
+<header id="gtco-header" class="gtco-cover" role="banner">
+    <div class="gtco-container">
+        <div class="row">
+            <div class="col-md-12 col-md-offset-0 text-left">
+                <div class="display-t">
+                    <div class="display-tc">
+                        <div class="row">
+                            <div class="col-md-5 text-center header-img animate-box">
+                                <img src="images/cube.png" alt="Free HTML5 Website Template by FreeHTML5.co">
+                            </div>
+                            <div class="col-md-7 copy animate-box">
+                                <h1>売り文句</h1>
+                                <p>超イケイケのサービスですっせ</p>
+                                <p><a href="" target="_blank" class="btn btn-white">お店を探してみる</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
+<div class="gtco-section gtco-testimonial gtco-gray">
+    <div class="gtco-container container">
+
+        <div class="row row-pb-sm">
+            <div class="col-md-8 col-md-offset-2 gtco-heading text-center">
+                <h2>かっこいい見出し</h2>
+                <p>こんな感じの流れですよ</p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1 animate-box">
+                <div class="gtco-testimony gtco-left">
+                    <div><img src="images/person_1.jpg" alt="Free Website template by FreeHTML5.co"></div>
+                    <blockquote>
+                        <p>まずは家を決めましょう</p>
+                    </blockquote>
+                </div>
+            </div>
+
+            <div class="col-md-10 col-md-offset-1 animate-box">
+                <div class="gtco-testimony gtco-left">
+                    <div><img src="images/person_1.jpg" alt="Free Website template by FreeHTML5.co"></div>
+                    <blockquote>
+                        <p>そんで家具決めましょう</p>
+                    </blockquote>
+                </div>
+            </div>
+
+            <div class="col-md-10 col-md-offset-1 animate-box">
+                <div class="gtco-testimony gtco-left">
+                    <div><img src="images/person_1.jpg" alt="Free Website template by FreeHTML5.co"></div>
+                    <blockquote>
+                        <p>内見日を決めて、お店へ</p>
+                    </blockquote>
+                </div>
+            </div>
+
+            <div class="col-md-10 col-md-offset-1 animate-box">
+                <div class="gtco-testimony gtco-left">
+                    <div><img src="images/person_1.jpg" alt="Free Website template by FreeHTML5.co"></div>
+                    <blockquote>
+                        <p>契約終了後、宅配業者が内装通りに家具を運んでくれます</p>
+                    </blockquote>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="gtco-section gtco-products">
+    <div class="gtco-container">
+        <div class="row row-pb-sm">
+            <div class="col-md-8 col-md-offset-2 text-center">
+                <h2>実際に探してみよう</h2>
+                <p>みようみよう</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <p><a href="" target="_blank" class="btn btn-special">お部屋を探す</a></p>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<script type="text/javascript">
+    $(function() {
+        $('nav ul li').eq(0).addClass('active');
+    });
+</script>
+@endsection
