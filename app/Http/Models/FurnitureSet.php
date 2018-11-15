acf2d3984915d9ae23b009f8376604983f3f434c
Yu Ishiguro <yuuis.egr@gmail.com>
2018-11-15 15:23:58 +0900

    add method to get fsf_path

--- app/Http/Models/FurnitureSet.php
+++ app/Http/Models/FurnitureSet.php
@@ -10,4 +10,9 @@ class FurnitureSet extends Model
     {
         return $this->belongsToMany('App\Http\Models\Furniture');
     }
+
+    public function fsfPath(Int $roomId)
+    {
+        return \DB::select("SELECT fsf_path FROM room_furniture_set WHERE room_id  = ? and furniture_set_id = ?", [$roomId, $this->id]);
+    }
 }
