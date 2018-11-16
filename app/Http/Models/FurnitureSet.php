956dbc0bbd0d63f6a81c4f7c26407bba9876b94e
Yu Ishiguro <yuuis.egr@gmail.com>
2018-11-16 12:06:46 +0900

    remove extra space

--- app/Http/Models/FurnitureSet.php
+++ app/Http/Models/FurnitureSet.php
@@ -13,6 +13,6 @@ class FurnitureSet extends Model
 
     public function fsfPath(Int $roomId)
     {
-        return \DB::select("SELECT fsf_path FROM room_furniture_set WHERE room_id  = ? and furniture_set_id = ?", [$roomId, $this->id]);
+        return \DB::select("SELECT fsf_path FROM room_furniture_set WHERE room_id = ? and furniture_set_id = ?", [$roomId, $this->id]);
     }
 }
