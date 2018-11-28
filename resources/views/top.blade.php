df42280ba273e9d9d3f9795626a5b951b9230529
Yu Ishiguro <yuuis.egr@gmail.com>
2018-11-29 07:52:36 +0900

    adjust wording on top page

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -41,7 +41,7 @@ Main Page
         <div class="row">
             <div class="col-md-10 col-md-offset-1 animate-box">
                 <div class="gtco-testimony gtco-left">
-                    <div><img src="images/person_1.jpg" alt="Free Website template by FreeHTML5.co"></div>
+                    <div><img src="https://suumo.jp/article/oyakudachi/wp-content/uploads/2017/09/uwamonokakaku_sub02.png" alt="Free Website template by FreeHTML5.co"></div>
                     <blockquote>
                         <h2>お部屋を決める</h2>
                         <p>まずはお部屋を決めましょう。xxx件以上のお部屋が掲載されています。</p>
@@ -51,7 +51,7 @@ Main Page
 
             <div class="col-md-10 col-md-offset-1 animate-box">
                 <div class="gtco-testimony gtco-left">
-                    <div><img src="images/person_1.jpg" alt="Free Website template by FreeHTML5.co"></div>
+                    <div><img src="http://magamit.com/wp-content/uploads/2017/03/furniture_2.png" alt="Free Website template by FreeHTML5.co"></div>
                     <blockquote>
                         <h2>レンタルする家具を決める</h2>
                         <p>Sotsuでは家具をレンタルすることができます。家具をレンタルすると、新しく家具を買う必要もなければ、次の引越しの時に持ち帰る必要もありません。</p>
@@ -61,7 +61,7 @@ Main Page
 
             <div class="col-md-10 col-md-offset-1 animate-box">
                 <div class="gtco-testimony gtco-left">
-                    <div><img src="images/person_1.jpg" alt="Free Website template by FreeHTML5.co"></div>
+                    <div><img src="https://4.bp.blogspot.com/-WC76L9341Vo/WTd4WkmtdtI/AAAAAAABEnI/czGKTcYwPjczZDkElyvkHImb8qc5JrukQCLcB/s800/bunbougu_fudepen.png"></div>
                     <blockquote>
                         <h2>お店にって契約をする</h2>
                         <p>実際にお店に行って内見をしましょう。実際にお部屋を見て気に入ったなら契約をします。</p>
@@ -71,7 +71,7 @@ Main Page
 
             <div class="col-md-10 col-md-offset-1 animate-box">
                 <div class="gtco-testimony gtco-left">
-                    <div><img src="images/person_1.jpg" alt="Free Website template by FreeHTML5.co"></div>
+                    <div><img src="http://www.silhouette-ac.com/sozai/m/151/89/151895m.jpg" alt="Free Website template by FreeHTML5.co"></div>
                     <blockquote>
                         <h2>入居する</h2>
                         <p>入居日には、家具が配置された状態のお部屋があなたを待っています。</p>
@@ -90,7 +90,7 @@ Main Page
         </div>
         <div class="row">
             <div class="col-md-12 text-center">
-                <p><a href="" target="_blank" class="btn btn-special">お部屋を探してみる</a></p>
+            <p><a href="{{url('rooms')}}" class="btn btn-special">お部屋を探してみる</a></p>
             </div>
         </div>
     </div>
